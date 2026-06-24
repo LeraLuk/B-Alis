388df329457436625c38167185336b4e20f7ea78
LeraLuk <valeriyalukomskaa@gmail.com>
2026-06-24 22:42:33 +0300

    Fix manifest: add PNG icons for PWA

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -15,16 +15,6 @@ declare module '*.png' {
   export default content;
 }
 
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.jpeg' {
-  const content: string;
-  export default content;
-}
-
 declare module '*.woff2' {
   const content: string;
   export default content;
